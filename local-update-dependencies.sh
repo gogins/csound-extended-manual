a5eef17d97a091a160ee3f9051ec3c88dd079e8a
Michael Gogins <michael.gogins@gmail.com>
2021-01-31 22:18:36 -0500

    No comment.

--- local-update-dependencies.sh
+++ local-update-dependencies.sh
@@ -4,7 +4,10 @@ echo "Updating WebAssemby dependencies for csound-examples from my home director
 cp -rf ~/csound-extended/WebAssembly/dist-wasm/*.js ./docs/examples/
 cp -rf ~/csound-extended/WebAssembly/dist-wasm/*.map ./docs/examples/
 cp -rf ~/csound-extended/WebAssembly/dist-wasm/*.wasm ./docs/examples/
+cp ~/csound-examples/docs/csound_loader.js ./docs/
+cp ~/csound-examples/docs/player.html ./docs/
 
 find . -name "*.js" -ls
 find . -name "*.wasm" -ls
+find . -name "player.html" -ls
 echo "Finished updating all WebAssembly dependencies for csound-examples."
